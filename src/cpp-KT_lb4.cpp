4aa0faf7fe8feb0c4b8b8ecc354aee92ea4d46c7
TGion <tobias@gion.io>
2020-07-28 10:48:40 +0200

    Initial commit: project created

---
+++ src/cpp-KT_lb4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-KT_lb4.cpp
+// Author      : Tobias Gion
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "cpp-KT_lb4" << endl; // prints cpp-KT_lb4
+	return 0;
+}
